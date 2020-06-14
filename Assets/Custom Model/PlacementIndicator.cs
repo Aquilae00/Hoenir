019e089983b59dab331f93d45c1a7bb29a6eaf28
Aquilae00 <linaldi03@gmail.com>
2020-06-13 23:13:46 -0400

    added indicator and temp model

--- Assets/Custom Model/PlacementIndicator.cs
+++ Assets/Custom Model/PlacementIndicator.cs
@@ -2,44 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleARCore;
+using UnityEngine.UI;
+using System.Linq;
 
 public class PlacementIndicator : MonoBehaviour
 {
 
     public Camera firstPersonCamera;
+    //prefabs
     public GameObject placementIndicator;
+    public GameObject placementIndicatorParticle;
     public GameObject model;
 
+    //objects
+    private GameObject placementIndicatorVar;
+    private GameObject placementIndicatorFixed;
+    private GameObject placedModel;
 
     private Pose placementPose;
     private bool placementPoseIsValid = false;
     private bool exist;
+    private TrackableHit hit;
+    private const float k_PrefabRotation = 180.0f;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        Button button = GameObject.Find("ResetButton").GetComponent<Button>();
+        button.onClick.AddListener(ResetAll);
+        init();
+    }
+    private void init()
+    {
         Debug.Log("Script Started");
-        placementIndicator = Instantiate(placementIndicator);
+        placementIndicatorVar = Instantiate(placementIndicator);
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        UpdatePlacementPose();
-        UpdatePlacementIndicator();
-        //PlacementSetup();
+        if(exist) { return; }
+        if (placementIndicatorVar != null)
+        {
+            UpdatePlacementPose();
+            UpdatePlacementIndicator();
+        } 
+        else if (placementIndicatorFixed != null)
+        {
+            PlacementSetup();
+        }
+
+        
     }
 
     private void PlacementSetup()
     {
-        //TrackableHit hit;
-        //TrackableHitFlags raycastFilter = TrackableHitFlags.Default;
-        //if (Frame.Raycast())
-        //{
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
 
-        //}
+
+        placedModel = Instantiate(model, placementIndicatorFixed.transform.position, placementIndicatorFixed.transform.rotation);
+
+        var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+        //placedModel.transform.Rotate(0, k_PrefabRotation, 0, Space.Self);
+        placedModel.transform.parent = anchor.transform;
+
+        exist = true;
     }
 
     private void UpdatePlacementIndicator()
@@ -47,20 +80,35 @@ public class PlacementIndicator : MonoBehaviour
         if(placementPoseIsValid)
         {
             Debug.Log("indicator placement valid");
-            placementIndicator.SetActive(true);
+            placementIndicatorVar.SetActive(true);
             Debug.Log(placementPose.position);
-            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+            placementIndicatorVar.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+
+            Touch touch;
+            if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+            {
+                return;
+            }
+
+
+            Destroy(placementIndicatorVar);
+            //var baseObject = Instantiate(placementIndicator, placementIndicator.transform.position, placementIndicator.transform.rotation);
+            placementIndicatorFixed = Instantiate(placementIndicatorParticle, placementPose.position, placementPose.rotation);
+
+            var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+            placementIndicatorFixed.transform.Rotate(0, k_PrefabRotation, 0, Space.Self);
+            placementIndicatorFixed.transform.parent = anchor.transform;
+
         }
         else
         {
-            placementIndicator.SetActive(false);
+            placementIndicatorVar.SetActive(false);
         }
     }
 
     private void UpdatePlacementPose()
     {
         var screenCenter = firstPersonCamera.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-        TrackableHit hit;
         TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinInfinity;
         if (Frame.Raycast(screenCenter.x, screenCenter.y, raycastFilter, out hit))
         {
@@ -73,4 +121,25 @@ public class PlacementIndicator : MonoBehaviour
             }
         }
     }
+
+
+    private void ResetAll()
+    {
+        exist = false;
+
+        //slow
+        foreach (GameObject o in Object.FindObjectsOfType<GameObject>())
+        {
+            if (o == placementIndicatorFixed ||
+                o == placementIndicatorVar ||
+                o == placedModel)
+            {
+                Destroy(o);
+            }
+
+        }
+
+        init();
+    }
+
 }
